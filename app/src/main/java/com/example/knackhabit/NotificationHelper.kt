eb84c7822894f68f8f2f4e3aa0dc04ccfaf76143
Em1l24 <130221713+Em1l24@users.noreply.github.com>
2025-05-11 20:10:25 +0500

    Update

--- app/src/main/java/com/example/knackhabit/NotificationHelper.kt
+++ app/src/main/java/com/example/knackhabit/NotificationHelper.kt
@@ -12,6 +12,7 @@ import com.example.knackhabit.activity.HomeActivity
 import com.example.knackhabit.db.HabitEntity
 import java.util.Calendar
 
+
 object NotificationHelper {
     const val CHANNEL_ID = "login_channel" // Должен соответствовать ID из App.kt
     private const val NOTIFICATION_ID = 1 // Уникальный ID уведомления
@@ -23,10 +24,16 @@ object NotificationHelper {
             "showLoginNotification() called with userName: $userName"
         )
         val intent = Intent(context, HomeActivity::class.java).apply {
-            flags = Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_SINGLE_TOP // ИЗМЕНЕНИЕ: Используйте CLEAR_TOP и SINGLE_TOP
+            flags =
+                Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_SINGLE_TOP // ИЗМЕНЕНИЕ: Используйте CLEAR_TOP и SINGLE_TOP
         }
         val pendingIntent: PendingIntent =
-            PendingIntent.getActivity(context, 0, intent, PendingIntent.FLAG_IMMUTABLE) //  Убедитесь, что флаг корректный
+            PendingIntent.getActivity(
+                context,
+                0,
+                intent,
+                PendingIntent.FLAG_IMMUTABLE
+            ) //  Убедитесь, что флаг корректный
 
         val builder = NotificationCompat.Builder(context, CHANNEL_ID)
             .setSmallIcon(R.mipmap.ic_notification)
@@ -40,8 +47,10 @@ object NotificationHelper {
         with(NotificationManagerCompat.from(context)) {
             try {
                 Log.d(
-                    "NotificationHelper", "Attempting to show notification with ID: $NOTIFICATION_ID")
-                notify(NOTIFICATION_ID, builder.build())
+                    "NotificationHelper",
+                    "Attempting to show notification with ID: $NOTIFICATION_ID"
+                )
+
                 Log.d(
                     "NotificationHelper",
                     "Notification shown successfully"
@@ -118,8 +127,10 @@ object NotificationHelper {
             val alarmInfo = AlarmManager.AlarmClockInfo(cal.timeInMillis, pi)
             alarmManager.setAlarmClock(alarmInfo, pi)
 
-            Log.d("NotificationHelper",
-                "Scheduled '${habit.title}' на $abbrev ($targetDow) в $hour:$minute, rc=$requestCode")
+            Log.d(
+                "NotificationHelper",
+                "Scheduled '${habit.title}' на $abbrev ($targetDow) в $hour:$minute, rc=$requestCode"
+            )
         }
     }
 
